a181785b030d39f17c39ea7dc0d1f509e08df531
royj18 <34381701+royj18@users.noreply.github.com>
2018-04-13 14:10:24 -0400

    Update SoundboardActivity.java

--- app/src/main/java/space/itsjosh/soundboard/SoundboardActivity.java
+++ app/src/main/java/space/itsjosh/soundboard/SoundboardActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -14,6 +15,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SoundboardActivity extends AppCompatActivity {
+
+    @SuppressWarnings("unused")
+    private enum Sounds {
+        audio1(),
+        audio2(),
+        audio3(),
+        audio4(),
+        audio5()
+    }
 
     Toolbar toolbar;
 
@@ -40,17 +50,34 @@ public class SoundboardActivity extends AppCompatActivity {
         List<String> nameList = Arrays.asList(getResources().getStringArray(R.array.soundNames));
 
         // Parse mp3's and resource names into a tidy object passed system
-        SoundObject[] soundItems = {
-                new SoundObject(nameList.get(0), R.raw.audio01),
-                new SoundObject(nameList.get(1), R.raw.audio02),
-                new SoundObject(nameList.get(2), R.raw.audio03),
-                new SoundObject(nameList.get(3), R.raw.audio04),
-                new SoundObject(nameList.get(4), R.raw.audio05),
-                new SoundObject(nameList.get(5), R.raw.audio06),
-                new SoundObject(nameList.get(6), R.raw.audio07),
-                new SoundObject(nameList.get(7), R.raw.audio08)};
 
-        soundList.addAll(Arrays.asList(soundItems));
+        int cIndex = 0;
+        int tIndex = Sounds.values().length;
+
+        ArrayList<SoundObject> soundItems = new ArrayList<>();
+
+        while (cIndex != tIndex - 1) {
+            int aIdInt = cIndex + 1;
+            String aId = String.valueOf(aIdInt);
+            if (aIdInt < 10)
+                aId = "0" + aId;
+            soundItems.add(new SoundObject(nameList.get(cIndex), getResources().getIdentifier("raw/audio" + aId, "raw", this.getPackageName())));
+            Log.i("SoundLoader", "Loaded Sound: " + aId + ". With Name: " + nameList.get(cIndex));
+            cIndex++;
+        }
+
+
+//        SoundObject[] soundItems = {
+//                new SoundObject(nameList.get(0), R.raw.audio01),
+//                new SoundObject(nameList.get(1), R.raw.audio01),
+//                new SoundObject(nameList.get(2), R.raw.audio02),
+//                new SoundObject(nameList.get(3), R.raw.audio04),
+//                new SoundObject(nameList.get(4), R.raw.audio03),
+//                new SoundObject(nameList.get(5), R.raw.audio04),
+//                new SoundObject(nameList.get(6), R.raw.audio05),
+//                new SoundObject(nameList.get(7), R.raw.audio08)};
+
+        soundList.addAll(soundItems);
 
         SoundView = findViewById(R.id.soundboardRecyclerView);
 
